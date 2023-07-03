3748077805afd356f0e6bdc4e93906f0aec5cec1
Gabriel Lombardi <gabriellomba@gmail.com>
2023-07-03 08:24:53 -0300

    Set shutdown timer in Windows

--- shutdown.py
+++ shutdown.py
@@ -1,7 +1,7 @@
 import os
 from datetime import timedelta, datetime
 
-timer = int(input('How long until shutdown (in hours)? '))
+timer = int(input('How long until shutdown (in hours)?  '))
 
 timer_delta = timedelta(hours=timer)
 timer_seconds = timer_delta.total_seconds()
