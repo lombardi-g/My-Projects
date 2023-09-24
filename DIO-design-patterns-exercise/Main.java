76481814cf7f8f571e0fc0ddf4f24dbaa26bdfd2
Gabriel Lombardi <gabriellomba@gmail.com>
2023-09-24 20:05:52 -0300

    DIO bootcamp facade project

--- DIO-design-patterns-exercise/Main.java
+++ DIO-design-patterns-exercise/Main.java
@@ -14,7 +14,7 @@ class Main
 }
 
 /**
- * Facade means hiding all the compexities from the client. In this example all the compelexity like switching on different system 
+ * Facade means hiding all the complexities from the client. In this example all the compelexity like switching on different system 
  * for the auto pilot to work has been done by the auto pilot class whcih is a facade; if it hadnot been there to make it work
  * client had to start all the system components himself.
  */
