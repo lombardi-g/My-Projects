86de842f3826976537e01306931d063a80a663b7
lombardi-g <gabriellomba@gmail.com>
2023-10-22 18:24:50 -0300

    Started excel export function

--- venv/figueirascraping.py
+++ venv/figueirascraping.py
@@ -6,6 +6,8 @@ import datetime
 # import tkinter as tk
 # from tkinter import messagebox
 from bs4 import BeautifulSoup
+import openpyxl
+from openpyxl import load_workbook
 
 # def defineURL(): #set URL everytime the program runs. use tkinter?
 #     entry = tk.Entry()
@@ -13,6 +15,53 @@ from bs4 import BeautifulSoup
 
 def caps_lock_ignore(text):
     return re.compile(text,re.I)
+                      
+def pass_to_excel():
+    workbook = load_workbook("Banco de Dados Figueirense Base.xlsx")
+    sheet = workbook['Jogos']
+    # Assigned labels from sheet. Read them and dynamically make the dict?
+    column_labels = {
+        "CÓDIGO JOGO":1,
+        "DATA JOGO":2,
+        "TREINADOR":3,
+        "CATEGORIA":4,
+        "COMPETIÇÃO":6,
+        "FIGUEIRENSE":7,
+        "G.F.":8,
+        "G.A.":9,
+        "ADVERSÁRIO":10,
+        "MANDO":11,
+        "LOCAL":12,
+        "Cidade":13,
+        "UF":14,
+        "JOGOS":15,
+        "VITÓRIA":16,
+        "EMPATE":17,
+        "DERROTA":18,
+        "MINUTOS JOGADOS":19,
+        "1º A MARCAR FIGUEIRENSE":20,
+        "1º A MARCAR ADVERSÁRIO":21,
+        "GOLS MARCADOS 1ºT - 0'-15'":22,
+        "GOLS MARCADOS 1ºT - 15'-30'":23,
+        "GOLS MARCADOS 1ºT - 30'-45'":24,
+        "GOLS MARCADOS 2ºT - 0'-15'":25,
+        "GOLS MARCADOS 2ºT - 15'-30'":26,
+        "GOLS MARCADOS 2ºT - 30'-45'":27,
+        "GOLS SOFRIDOS 1ºT - 0'-15'":28,
+        "GOLS SOFRIDOS 1ºT - 15'-30'":29,
+        "GOLS SOFRIDOS 1ºT - 30'-45'":30,
+        "GOLS SOFRIDOS 2ºT - 0'-15'":31,
+        "GOLS SOFRIDOS 2ºT - 15'-30'":32,
+        "GOLS SOFRIDOS 2ºT - 30'-45'":33
+    }
+    last_row = sheet.max_row
+    last_row_value = sheet.cell(row=last_row, column=column_labels['CÓDIGO JOGO']).value
+    print(last_row)
+    print(last_row_value)
+    # new_row = last_row + 1
+    # sheet.cell(row=last_row, column=column_labels['CÓDIGO JOGO'],)
+
+    # workbook.save("Banco de Dados Figueirense Base.xlsx")
 
 url = 'https://egol.fcf.com.br/SISGOL/WDER0700_Sumula.asp?SelStart1=2023&SelStop1=2023&SelStart2=557&SelStop2=557&SelStart3=16&SelStop3=16&Index=1&RunReport=Run+Report'
 response = requests.get(url)
@@ -36,7 +85,7 @@ else:
 
 # Find category
 full_tournament = targetURL.find(string=caps_lock_ignore('sub-'))
-category = "SUB-"+full_tournament[full_tournament.upper().find("SUB-")+4:full_tournament.upper().find("SUB-")+6]
+category = "Sub"+full_tournament[full_tournament.upper().find("SUB-")+4:full_tournament.upper().find("SUB-")+6]
 
 # Which round
 round_number = full_tournament.find_next(string='Rodada:').find_next()
@@ -61,7 +110,7 @@ print(figueira_final_score)
 print(opponent_final_score)
 print(round_number)
 print(category)
-
 print(date)
 print(time)
 print(place)
+pass_to_excel()
