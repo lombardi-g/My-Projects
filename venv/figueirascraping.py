0018a53fc44019e19703cd7a27bff81fa0292071
lombardi-g <gabriellomba@gmail.com>
2023-10-23 11:37:37 -0300

    Goal info scraping added

--- venv/figueirascraping.py
+++ venv/figueirascraping.py
@@ -8,7 +8,7 @@ import re
 # from tkinter import messagebox
 from bs4 import BeautifulSoup
 
-# def defineURL(): #set URL everytime the program runs. use tkinter?
+# def defineURL(): #set URL everytime the program runs. use tkinter? verify if main domain is fcf
 #     entry = tk.Entry()
 #     site = entry.get()
 
@@ -53,6 +53,14 @@ def pass_to_excel():
         "GOLS SOFRIDOS 2ºT - 15'-30'":32,
         "GOLS SOFRIDOS 2ºT - 30'-45'":33
     }
+    staff = {
+        "Sub14":"Guilherme Pereira",
+        "Sub15":"Guilherme Pereira",
+        "Sub17":"Lucas Ligio",
+        "Sub20":"Jhonatas Reis",
+        "Sub21":"Jhonatas Reis"
+    }
+
     last_row = sheet.max_row
     last_row_value = 2
     while last_row > 1:
@@ -66,10 +74,29 @@ def pass_to_excel():
     print(last_row_value)
     new_row = last_row + 1
     sheet.cell(row=new_row, column=column_labels['CÓDIGO JOGO'],value=last_row_value+1)
+    sheet.cell(row=new_row, column=column_labels['DATA JOGO'],value=date)
+    sheet.cell(row=new_row, column=column_labels['TREINADOR'],value=staff[category])
+    sheet.cell(row=new_row, column=column_labels['CATEGORIA'],value=category)
+    sheet.cell(row=new_row, column=column_labels['COMPETIÇÃO'],value=tournament)
+    sheet.cell(row=new_row, column=column_labels['FIGUEIRENSE'],value="Figueirense")
+    sheet.cell(row=new_row, column=column_labels['G.F.'],value=figueira_final_score)
+    sheet.cell(row=new_row, column=column_labels['G.A.'],value=opponent_final_score)
+    sheet.cell(row=new_row, column=column_labels['ADVERSÁRIO'],value=opponent)
+    sheet.cell(row=new_row, column=column_labels['MANDO'],value=home)
+    sheet.cell(row=new_row, column=column_labels['LOCAL'],value=place)
+    # sheet.cell(row=new_row, column=column_labels['Cidade'],value=)
+    # sheet.cell(row=new_row, column=column_labels['UF'],value=)
+    # sheet.cell(row=new_row, column=column_labels['JOGOS'],value=1)
+    # sheet.cell(row=new_row, column=column_labels['VITÓRIA'],value=1 if figueira_final_score > opponent_final_score else 0)
+    # sheet.cell(row=new_row, column=column_labels['EMPATE'],value=1 if figueira_final_score == opponent_final_score else 0)
+    # sheet.cell(row=new_row, column=column_labels['DERROTA'],value=1 if figueira_final_score < opponent_final_score else 0)
+    # sheet.cell(row=new_row, column=column_labels['MINUTOS JOGADOS'],value=)
+    # sheet.cell(row=new_row, column=column_labels['1º A MARCAR FIGUEIRENSE'],value=1 if figueira_first else 0)
+    # sheet.cell(row=new_row, column=column_labels['1º A MARCAR ADVERSÁRIO'],value=1 if figueira_first else 0)
 
     workbook.save("Banco de Dados Figueirense Base.xlsx")
 
-url = 'https://egol.fcf.com.br/SISGOL/WDER0700_Sumula.asp?SelStart1=2023&SelStop1=2023&SelStart2=557&SelStop2=557&SelStart3=16&SelStop3=16&Index=1&RunReport=Run+Report'
+url = 'https://egol.fcf.com.br/SISGOL/WDER0700_Sumula.asp?SelStart1=2023&SelStop1=2023&SelStart2=505&SelStop2=505&SelStart3=9&SelStop3=9&Index=1&RunReport=Run+Report'
 response = requests.get(url)
 targetURL = BeautifulSoup(response.text, 'html.parser')
 
@@ -82,16 +109,19 @@ opponent = [team for team in teams if team.upper() != 'FIGUEIRENSE'][0]
 final_score = match.find_next('p').find_next()
 final_score = final_score.get_text().split(' x ')
 if teams[0] == "FIGUEIRENSE":
-    figueira_final_score = final_score[0]
-    opponent_final_score = final_score[1]
+    home = "Casa"
+    figueira_final_score = int(final_score[0])
+    opponent_final_score = int(final_score[1])
 else:
-    figueira_final_score = final_score[1]
-    opponent_final_score = final_score[0]
+    home = "Fora"
+    figueira_final_score = int(final_score[1])
+    opponent_final_score = int(final_score[0])
 
 
 # Find category
 full_tournament = targetURL.find(string=caps_lock_ignore('sub-'))
 category = "Sub"+full_tournament[full_tournament.upper().find("SUB-")+4:full_tournament.upper().find("SUB-")+6]
+tournament = "Campeonato Catarinense"
 
 # Which round
 round_number = full_tournament.find_next(string='Rodada:').find_next()
@@ -107,6 +137,35 @@ time = time_locator.find_next().get_text()
 place_locator = time_locator.find_next(string="Local:").find_next()
 place = place_locator.get_text()
 
+#Scoring information
+score_locator_beginning = targetURL.find(string=caps_lock_ignore("5.0 - GOLS"))
+score_locator_end = targetURL.find(name="td",string=caps_lock_ignore("6.0 - "))
+goal_reader = score_locator_beginning.findNext(name="td",string="Equipe")
+goal_minute_locator = goal_reader
+goals_info=[]
+total_goals=0
+if figueira_final_score or opponent_final_score != 0:
+    while goal_minute_locator is not score_locator_end:
+        goal_minute_locator = goal_reader.find_next(name="td")
+        goal_minute = goal_minute_locator.get_text()
+        goal_half_locator = goal_minute_locator.find_next(name="td")
+        goal_half = goal_half_locator.get_text()
+        goal_team_locator = goal_half_locator.find_next(name="td").find_next(name="td").find_next(name="td").find_next(name="td")
+        goal_team = goal_team_locator.get_text().capitalize()
+        if total_goals==0:
+            figueira_first = True if goal_team == "Figueirense" else False
+        goal_info = {
+            "minute":goal_minute,
+            "half":goal_half,
+            "team":goal_team
+        }
+        goals_info.append(goal_info)
+        total_goals+=1
+        goal_reader = goal_team_locator
+        goal_minute_locator = goal_reader.find_next(name="td")
+else:
+    figueira_first=False
+
 # tests
 # print (BeautifulSoup().PREFERRED_PARSER)
 # print (openpyxl.__version__)
@@ -119,4 +178,4 @@ print(category)
 print(date)
 print(time)
 print(place)
-pass_to_excel()
+# pass_to_excel()
