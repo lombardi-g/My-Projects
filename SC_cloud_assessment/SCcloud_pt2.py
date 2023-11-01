6440a7e81dd942f15785cf582c21fdd041efd6b3
lombardi-g <gabriellomba@gmail.com>
2023-11-01 12:59:28 -0300

    Commit answer to 2nd question

---
+++ SC_cloud_assessment/SCcloud_pt2.py
@@ -0,0 +1,43 @@
+# Como segunda etapa, gostaríamos de ver alguns códigos gerados por ti.
+# Sendo assim, pedimos que tu crie códigos para os seguintes desafios.
+# 1 - Fibonnaci
+#     -- Criar uma função em sua linguagem preferida. A função deve receber um numero N >= 0 (deve validar o input para a função), e retornar o valor correspondente desse número na sequencia fibonnaci. EX. fib(0) =0; fib(1) = 1; fib(2) = 1; fib(3) = 2; fib(5) = 5; fib(6) = 8.
+#     --- Criar uma função recursiva que resolva fibonacci
+#     --- Criar uma função linear que resolva fibonnaci
+
+def validate():
+    while True:
+        try:
+            fibonacci_n = int(input("Qual n-ésimo dígito da sequência de Fibonacci deseja? "))
+            if fibonacci_n > 0:
+                # print(fibonacci_n) #test
+                return fibonacci_n
+            else:
+                print("Inserir número positivo.")
+        except ValueError:
+            print("Inserir número positivo inteiro.")
+     
+def recursive_fibonacci(n):
+    if n==1 or n==2:
+        return 1
+    else:
+        return recursive_fibonacci(n-1) + recursive_fibonacci (n-2)
+
+def linear_fibonacci(n):     
+    if n==1 or n==2:
+        return 1
+    
+    current = 1
+    previous = 0
+    for _ in range(2,n+1):
+        new = current + previous
+        previous = current
+        current = new
+    return current
+
+valid_fibonacci = validate()
+print("\nResolução da sequência de Fibonacci por função recursiva em Python.")
+print(f'O valor do {valid_fibonacci}º número é {recursive_fibonacci(valid_fibonacci)}')
+
+print("\nResolução da sequência de Fibonacci por função linear em Python.")
+print(f'O valor do {valid_fibonacci}º número é {linear_fibonacci(valid_fibonacci)}\n')
