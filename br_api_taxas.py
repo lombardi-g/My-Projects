43fa06aab259015b1516b3edff9f11fb8c026d38
lombardi-g <gabriellomba@gmail.com>
2025-09-12 08:49:24 -0300

    accessing endpoint

--- br_api_taxas.py
+++ br_api_taxas.py
@@ -1,1 +1,13 @@
+import requests
+from datetime import datetime
 
+current_time = datetime.now()
+formatted_time = current_time.strftime("%d/%m/%Y")
+taxas = "https://brasilapi.com.br/api/taxas/v1"
+
+response = requests.get(taxas)
+data = response.json()
+
+print(f'Response at {formatted_time}: {response.status_code}')
+for _ in data:
+    print (_)
